223a0932d13b9dad2caee4776ff75d8a8538d971
Paul Cheney <spartandesign.biz@gmail.com>
2025-11-29 15:26:09 -0700

    sunrise

--- modules/current-weather.mjs
+++ modules/current-weather.mjs
@@ -15,13 +15,15 @@ export async function getCurrentWeather(lat, lon) {
     console.log(weatherInfo)
     console.log(weatherInfo.weather[0].icon)
     currentTemp.innerText = `${Math.floor(weatherInfo.main.temp)}°`
-//
+    //
     currentConditions.innerHTML = `
     <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" width="1" height="1" alt="weather icon">
     <p>${weatherInfo.weather[0].description}</p>
     <p>LOW: ${Math.floor(weatherInfo.main.temp_max)}° - HIGH: ${Math.floor(weatherInfo.main.temp_min)}°</p>
     <p>Wind Speed: ${weatherInfo.wind.speed}mph</p>
     <p>Wind Gusts: ${weatherInfo.wind.gust}mph</p>
+    <p>Sunrise: ${convert(weatherInfo.sys.sunrise)}</p>
+    <p>Sunset: ${convert(weatherInfo.sys.sunset)}</p>
     `
 
   } catch (err) {
@@ -29,4 +31,13 @@ export async function getCurrentWeather(lat, lon) {
   }
 }
 
+// convert Unix timestamp to military time
+function convert(tms) {
+  const ts = tms * 1000; // convert to milliseconds
+  const date = new Date(ts);
 
+  const hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  return `${hours}:${minutes}`
+  console.log(`${hours}:${minutes}`);
+}
